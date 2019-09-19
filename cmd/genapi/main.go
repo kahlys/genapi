5a3d74145d0d864cbb2b66ed3e3f6d1b3d0fcb8a
kahlys <youennlaborde@live.fr>
2019-09-20 00:08:35 +0200

    use config file instead of command line

--- cmd/genapi/main.go
+++ cmd/genapi/main.go
@@ -1,288 +1,60 @@
 package main
 
 import (
-	"bytes"
+	"flag"
 	"fmt"
-	"go/format"
-	"html/template"
-	"os"
+	"log"
+	"path/filepath"
 	"strings"
 
-	"github.com/abiosoft/ishell"
+	"github.com/kahlys/genapi"
+	"github.com/spf13/viper"
 )
 
-var version = "undefined"
-
-var datas struct {
-	directory   string
-	serviceName string
-	endpoints   []Endpoint
-}
+// flags
+var (
+	version  = "undefined"
+	fversion = flag.Bool("version", false, "print server version")
+	fconfig  = flag.String("config", "", "configuration file path")
+	fdir     = flag.String("dir", "", "output directory")
+)
 
 // TODO add database structure (optional)
-// TODO add main file
-// TODO structure with dir cmd and dir pkg
 
-// TODO make configurable
-const (
-	pkgname = "main"
-)
+var restapi genapi.RestAPI
 
-func main() {
-	shell := ishell.New()
-
-	// display info.
-	shell.Println("Genapi - version", version)
-
-	// configure
-	shell.AddCmd(&ishell.Cmd{
-		Name: "config",
-		Func: func(c *ishell.Context) {
-			c.ShowPrompt(false)
-			defer c.ShowPrompt(true)
-			c.Println("Configure service")
-			c.Print("Service name: ")
-			servicename := c.ReadLine()
-			c.Print("Destination directory: ")
-			dir := c.ReadLine()
-			// TODO verify name (no space, etc)
-			datas.serviceName = servicename
-			datas.directory = dir
-		},
-		Help: "global configuration",
-	})
-
-	// add endpoint
-	shell.AddCmd(&ishell.Cmd{
-		Name: "add",
-		Func: func(c *ishell.Context) {
-			c.ShowPrompt(false)
-			defer c.ShowPrompt(true)
-			c.Println("Add an endpoint")
-			e := Endpoint{}
-			c.Print("Name: ")
-			e.Name = c.ReadLine()
-			c.Print("URL: ")
-			e.URL = c.ReadLine()
-			c.Print("Method: ")
-			e.Method = c.ReadLine()
-			// TODO verify endpoint before adding it to the list
-			c.Println("Adding", e.Name, ":", e.Method, e.URL)
-			datas.endpoints = append(datas.endpoints, e)
-		},
-		Help: "add an endpoint",
-	})
-
-	// show configuration
-	shell.AddCmd(&ishell.Cmd{
-		Name: "show",
-		Func: func(c *ishell.Context) {
-			c.ShowPrompt(false)
-			defer c.ShowPrompt(true)
-			// TODO proper display
-			c.Println(datas)
-		},
-		Help: "show configuration",
-	})
-
-	// show configuration
-	shell.AddCmd(&ishell.Cmd{
-		Name: "run",
-		Func: func(c *ishell.Context) {
-			c.ShowPrompt(false)
-			defer c.ShowPrompt(true)
-			if err := generate(); err != nil {
-				c.Println("ERROR:", err)
-			}
-		},
-		Help: "generate golang code",
-	})
-
-	shell.Run()
-	shell.Close()
-}
-
-func generate() error {
-	// create destination directory
-	if err := os.Mkdir(datas.directory, os.ModePerm); err != nil {
-		return fmt.Errorf("directory %v already exists", datas.directory)
+func parseConfig() error {
+	viper.SetConfigName(strings.TrimSuffix(filepath.Base(*fconfig), filepath.Ext(filepath.Base(*fconfig))))
+	viper.AddConfigPath(filepath.Dir(*fconfig))
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("unable to read configuration file: %v", err)
 	}
-
-	// Service
-	service, err := writeService(pkgname, datas.serviceName, datas.endpoints...)
-	if err != nil {
-		return err
+	// Configuration for the micro-service
+	if err := viper.Unmarshal(&restapi); err != nil {
+		return fmt.Errorf("unable to decode into config struct: %v", err)
 	}
-	f, err := os.OpenFile(fmt.Sprintf("%v/service.go", datas.directory), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	defer f.Close()
-	if err != nil {
-		return err
-	}
-	_, err = f.Write(service)
-	if err != nil {
-		return err
-	}
-
-	// Handler
-	handler, err := writeHandler(pkgname, datas.serviceName, datas.endpoints...)
-	if err != nil {
-		return err
-	}
-	fhandler, err := os.OpenFile(fmt.Sprintf("%v/handler.go", datas.directory), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	defer fhandler.Close()
-	if err != nil {
-		return err
-	}
-	_, err = fhandler.Write(handler)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
-func writeService(pkg, name string, methods ...Endpoint) ([]byte, error) {
-	pkg = strings.ToLower(pkg)
-	name = strings.Title(strings.ToLower(name))
-
-	// package header
-	buf := bytes.NewBufferString(fmt.Sprintf("package %v\n", pkg))
-
-	// write structure
-	t := Type{
-		Comments: fmt.Sprintf("%v ...", name),
-		Name:     name,
-	}
-	if err := tmplTypes.Execute(buf, t); err != nil {
-		return []byte{}, err
-	}
-
-	// write methods
-	for _, m := range methods {
-		funcname := strings.Title(strings.ToLower(m.Name))
-		meth := Method{
-			Recv: fmt.Sprintf("%v *%v", strings.Split(strings.ToLower(name), "")[0], name),
-			Func: Func{
-				Comments: fmt.Sprintf("%v ...", funcname),
-				Name:     funcname,
-			},
-		}
-		if err := tmplMethodes.Execute(buf, meth); err != nil {
-			return []byte{}, err
-		}
-	}
-
-	// format
-	buf = bytes.NewBufferString(strings.Replace(buf.String(), "&#34;", "\"", -1)) // TODO find a way to use quotes in template
-	pretty, err := format.Source(buf.Bytes())
-	if err != nil {
-		return []byte{}, err
-	}
-
-	return pretty, nil
+func verifyConfig() error {
+	return nil
 }
 
-func writeHandler(pkg, name string, methods ...Endpoint) ([]byte, error) {
-	pkg = strings.ToLower(pkg)
-	name = strings.Title(strings.ToLower(name))
-
-	// package header
-	buf := bytes.NewBufferString(fmt.Sprintf("package %v\n", pkg))
-	buf.WriteString("import( \"net/http\" \n\n \"github.com/gorilla/mux\" )\n")
-
-	// write methods
-	endpoints := []string{"r := mux.NewRouter()"}
-	for _, m := range methods {
-		funcname := fmt.Sprintf("handle%v", strings.Title(strings.ToLower(m.Name)))
-		recvName := strings.Split(strings.ToLower(name), "")[0]
-		endpoints = append(endpoints, fmt.Sprintf("r.HandleFunc(\"%s\", %s.%s)", m.URL, recvName, funcname))
-		meth := Method{
-			Recv: fmt.Sprintf("%v *%v", recvName, name),
-			Func: Func{
-				Comments: fmt.Sprintf("%v ...", funcname),
-				Name:     funcname,
-				Params: []Param{
-					{Name: "w", Type: "http.ResponseWriter"},
-					{Name: "req", Type: "*http.Request"},
-				},
-				Content: []string{fmt.Sprintf("%v.%v()", recvName, strings.Title(strings.ToLower(m.Name)))},
-			},
-		}
-		if err := tmplMethodes.Execute(buf, meth); err != nil {
-			return []byte{}, err
-		}
+func main() {
+	flag.Parse()
+	if *fversion {
+		fmt.Println(version)
+		return
 	}
-	endpoints = append(endpoints, fmt.Sprintf("return r"))
-
-	// write Handler method
-	meth := Method{
-		Recv: fmt.Sprintf("%v *%v", strings.Split(strings.ToLower(name), "")[0], name),
-		Func: Func{
-			Comments: fmt.Sprintf("Handler returns the %v HTTP Handler.", name),
-			Name:     "Handler",
-			Content:  endpoints,
-			Res:      []Param{{Type: "http.Handler"}},
-		},
+	// configuration
+	if err := parseConfig(); err != nil {
+		log.Fatal("unable to parse configuration file:", err)
 	}
-	if err := tmplMethodes.Execute(buf, meth); err != nil {
-		return []byte{}, err
+	if err := verifyConfig(); err != nil {
+		log.Fatal("missing configuration parameters:", err)
 	}
-
-	// format
-	buf = bytes.NewBufferString(strings.Replace(buf.String(), "&#34;", "\"", -1)) // TODO find a way to use quotes in template
-	pretty, err := format.Source(buf.Bytes())
-	if err != nil {
-		return []byte{}, err
+	// generation
+	if err := restapi.Generate(*fdir); err != nil {
+		panic(err)
 	}
-
-	return pretty, nil
 }
-
-// Endpoint http rest api
-type Endpoint struct {
-	Name   string
-	URL    string
-	Method string
-}
-
-// Method represents a method signature.
-type Method struct {
-	Recv string
-	Func
-}
-
-// Func represents a function signature.
-type Func struct {
-	Comments string
-	Name     string
-	Params   []Param
-	Res      []Param
-	Content  []string
-}
-
-// Param represents a parameter in a function or method signature.
-type Param struct {
-	Name string
-	Type string
-}
-
-// Type represents a type
-type Type struct {
-	Comments string
-	Name     string
-	Fields   []Param
-}
-
-const textMethods = "{{if .Comments}}// {{.Comments}}\n{{end}}" +
-	"func ({{.Recv}}) {{.Name}}" +
-	"({{range .Params}}{{.Name}} {{.Type}}, {{end}})" +
-	"({{range .Res}}{{.Name}} {{.Type}}, {{end}})" +
-	"{\n {{if .Content}} {{range $i, $x := .Content}} {{$x}}; {{end}} {{else}}panic(\"not implemented\"){{end}}}\n\n"
-
-var tmplMethodes = template.Must(template.New("methods").Parse(textMethods))
-
-const textTypes = "{{if .Comments}}// {{.Comments}}\n{{end}}" +
-	"type {{.Name}} struct" +
-	"{}\n\n"
-
-var tmplTypes = template.Must(template.New("types").Parse(textTypes))
---
+++ genapi.go
@@ -0,0 +1,205 @@
+package genapi
+
+import (
+	"bytes"
+	"fmt"
+	"go/format"
+	"os"
+	"strings"
+	"text/template"
+)
+
+// RestAPI description
+type RestAPI struct {
+	ServiceName string
+	Endpoints   []Endpoint
+}
+
+// Endpoint http rest api
+type Endpoint struct {
+	Name   string
+	URL    string
+	Method string
+}
+
+// Generate ...
+func (d RestAPI) Generate(dir string) error {
+	pkgname := strings.ToLower(d.ServiceName)
+
+	// create destination directory
+	if err := os.Mkdir(dir, os.ModePerm); err != nil {
+		return fmt.Errorf("directory %v already exists", dir)
+	}
+
+	// Service
+	service, err := writeService(pkgname, d.ServiceName, d.Endpoints...)
+	if err != nil {
+		return err
+	}
+	f, err := os.OpenFile(fmt.Sprintf("%v/service.go", dir), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(service)
+	if err != nil {
+		return err
+	}
+
+	// Handler
+	handler, err := writeHandler(pkgname, d.ServiceName, d.Endpoints...)
+	if err != nil {
+		return err
+	}
+	fhandler, err := os.OpenFile(fmt.Sprintf("%v/handler.go", dir), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer fhandler.Close()
+	if err != nil {
+		return err
+	}
+	_, err = fhandler.Write(handler)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeService(pkg, name string, methods ...Endpoint) ([]byte, error) {
+	pkg = strings.ToLower(pkg)
+	name = strings.Title(strings.ToLower(name))
+
+	// package header
+	buf := bytes.NewBufferString(fmt.Sprintf("package %v\n", pkg))
+
+	// write structure
+	t := Structure{
+		Comments: fmt.Sprintf("%v ...", name),
+		Name:     name,
+	}
+	if err := tmplTypes.Execute(buf, t); err != nil {
+		return []byte{}, err
+	}
+
+	// write methods
+	for _, m := range methods {
+		funcname := strings.Title(strings.ToLower(m.Name))
+		meth := Method{
+			Recv: fmt.Sprintf("%v *%v", strings.Split(strings.ToLower(name), "")[0], name),
+			Function: Function{
+				Comments: fmt.Sprintf("%v ...", funcname),
+				Name:     funcname,
+			},
+		}
+		if err := tmplMethodes.Execute(buf, meth); err != nil {
+			return []byte{}, err
+		}
+	}
+
+	// format
+	buf = bytes.NewBufferString(strings.Replace(buf.String(), "&#34;", "\"", -1)) // TODO find a way to use quotes in template
+	pretty, err := format.Source(buf.Bytes())
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return pretty, nil
+}
+
+func writeHandler(pkg, name string, methods ...Endpoint) ([]byte, error) {
+	pkg = strings.ToLower(pkg)
+	name = strings.Title(strings.ToLower(name))
+
+	// package header
+	buf := bytes.NewBufferString(fmt.Sprintf("package %v\n", pkg))
+	buf.WriteString("import( \"net/http\" \n\n \"github.com/gorilla/mux\" )\n")
+
+	// write methods
+	endpoints := []string{"r := mux.NewRouter()"}
+	for _, m := range methods {
+		funcname := fmt.Sprintf("handle%v", strings.Title(strings.ToLower(m.Name)))
+		recvName := strings.Split(strings.ToLower(name), "")[0]
+		endpoints = append(endpoints, fmt.Sprintf("r.HandleFunc(\"%s\", %s.%s).Methods(\"%v\")", m.URL, recvName, funcname, m.Method))
+		meth := Method{
+			Recv: fmt.Sprintf("%v *%v", recvName, name),
+			Function: Function{
+				Comments: fmt.Sprintf("%v ...", funcname),
+				Name:     funcname,
+				Params: []Parameter{
+					{Name: "w", Type: "http.ResponseWriter"},
+					{Name: "req", Type: "*http.Request"},
+				},
+				Content: []string{fmt.Sprintf("%v.%v()", recvName, strings.Title(strings.ToLower(m.Name)))},
+			},
+		}
+		if err := tmplMethodes.Execute(buf, meth); err != nil {
+			return []byte{}, err
+		}
+	}
+	endpoints = append(endpoints, fmt.Sprintf("return r"))
+
+	// write Handler method
+	meth := Method{
+		Recv: fmt.Sprintf("%v *%v", strings.Split(strings.ToLower(name), "")[0], name),
+		Function: Function{
+			Comments: fmt.Sprintf("Handler returns the %v HTTP Handler.", name),
+			Name:     "Handler",
+			Content:  endpoints,
+			Res:      []Parameter{{Type: "http.Handler"}},
+		},
+	}
+	if err := tmplMethodes.Execute(buf, meth); err != nil {
+		return []byte{}, err
+	}
+
+	// format
+	buf = bytes.NewBufferString(strings.Replace(buf.String(), "&#34;", "\"", -1)) // TODO find a way to use quotes in template
+	pretty, err := format.Source(buf.Bytes())
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return pretty, nil
+}
+
+// Method represents a method signature.
+type Method struct {
+	Recv string
+	Function
+}
+
+// Function represents a function signature.
+type Function struct {
+	Comments string
+	Name     string
+	Params   []Parameter
+	Res      []Parameter
+	Content  []string
+}
+
+// Parameter represents a parameter in a function or method signature.
+type Parameter struct {
+	Name string
+	Type string
+}
+
+// Structure represents a type
+type Structure struct {
+	Comments string
+	Name     string
+	Fields   []Parameter
+}
+
+const textMethods = "{{if .Comments}}// {{.Comments}}\n{{end}}" +
+	"func ({{.Recv}}) {{.Name}}" +
+	"({{range .Params}}{{.Name}} {{.Type}}, {{end}})" +
+	"({{range .Res}}{{.Name}} {{.Type}}, {{end}})" +
+	"{\n {{if .Content}} {{range $i, $x := .Content}} {{$x}}; {{end}} {{else}}panic(\"not implemented\"){{end}}}\n\n"
+
+const textTypes = "{{if .Comments}}// {{.Comments}}\n{{end}}" +
+	"type {{.Name}} struct" +
+	"{}\n\n"
+
+var (
+	tmplMethodes = template.Must(template.New("methods").Parse(textMethods))
+	tmplTypes    = template.Must(template.New("types").Parse(textTypes))
+)
